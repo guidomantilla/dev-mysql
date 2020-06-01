976c1e31852a088bfc6048225302243a4c453ceb
Guido Mauricio Mantilla Tarazona <guido.mantilla@yahoo.com>
2020-06-01 13:36:05 -0500

    FIx Commit

---
+++ scripts/prowide_swift-create.sql
@@ -0,0 +1,3 @@
+drop database if exists `prowide_swift`;
+create database `prowide_swift` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+use `prowide_swift`;
